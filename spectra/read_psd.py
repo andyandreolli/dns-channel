ac85483fa860b9e63ffd9530677b2ed162841049
andyandreolli <hi209@istmcapricornus.smarthost.kit.edu>
2020-07-10 17:41:41 +0200

    Summing kz directly in spectra/psd.cpl; also removing ghost cells

--- spectra/read_psd.py
+++ spectra/read_psd.py
@@ -1,8 +1,6 @@
 import numpy as np
 import tactical as tct
 import dns_channel as ch
-import itertools
-import progressbar
 
 
 
@@ -17,24 +15,18 @@ def read_psd(fdir, **kwargs):
     mesh = ch.mesh(dnsdict)
 
     # create memmap
-    diskacc = np.memmap(fname, mode='r', dtype=np.float64, shape=(6, mesh.ny+3, 2*mesh.nz+1, mesh.nx+1))
+    diskacc = np.memmap(fname, mode='r', dtype=np.float64, shape=(6, mesh.ny+1, mesh.nz+1, mesh.nx+1))
 
     # check file size and allocate memory
     tct.io.size_ram_check(fname)
     all_spectra = np.zeros((6, mesh.ny+1, mesh.nz+1, mesh.nx+1))
 
-    with progressbar.ProgressBar(max_value=6*(mesh.nz+1)*(mesh.ny+1)*(mesh.nx+1)) as bar:
-        for ii, (iy, iz) in enumerate(itertools.product(range(mesh.ny+1), range(mesh.nz+1))):
-            izb = -1-iz
-            izm = mesh.nz + izb
-            all_spectra[:, iy, izm, :] = diskacc[:, iy+1, iz, :] + diskacc[:, iy+1, izb, :]
-            if izm == 0:
-                all_spectra[:, iy, iz, :] = all_spectra[:, iy, iz, :]/2
-            bar.update(ii)
+    # load
+    all_spectra += diskacc
 
     # average on y (if requested)
     if y_symm:
-        all_spectra += all_spectra[:,::-1,:,:]
+        all_spectra += diskacc[:,::-1,:,:]
         all_spectra /= 2
 
     return all_spectra, mesh.kx, mesh.kz, mesh.y[1:-1]
